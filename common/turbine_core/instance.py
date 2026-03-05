f8a51cc8f37ca9957998134033ff0dffc2bda606
byk2002 <2165106527@qq.com>
2026-03-05 15:34:32 +0800

    Delete common directory

--- common/turbine_core/instance.py
+++
@@ -1,9 +0,0 @@
-from .turbine_system_integration_1 import TurbineMachinerySystem
-import os
-
-# 全局单例实例化系统
-turbine_system = TurbineMachinerySystem(
-    kb_path="./multimodel_doc_kb",
-    openai_api_key=os.getenv("OPENAI_API_KEY", "your-key"),
-    model_name="gpt-4o"
-)
